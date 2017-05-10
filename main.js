143228f3695cf95ea0275e10a3f1c498ea172753
wallytreats <nicklawrenceemail@gmail.com>
2017-05-10 13:26:44 -0600

    Colors working now, FunMode added w/ clear button

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $('#mainBtn').click(function(){
   $('#mainBtn').hide();
   $('header').hide();
   $('h4').hide();
-
+  $('#copyright').hide();
 
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var source;
@@ -10,8 +10,8 @@ var stream;
 
 //audio nodes configuration
 var analyser = audioCtx.createAnalyser();
-analyser.minDecibels = -75;
-analyser.maxDecibels = -25;
+analyser.minDecibels = -100;
+analyser.maxDecibels = 20;
 analyser.smoothingTimeConstant = 0.95;
 
 var gain = audioCtx.createGain();
@@ -25,10 +25,6 @@ var canvasCtx = canvas.getContext("2d");
 var intendedWidth = document.querySelector('.visualizer').clientWidth;
 
 canvas.setAttribute('width', intendedWidth);
-
-var intendedHeight = document.querySelector('.visualizer').clientHeight;
-
-canvas.setAttribute('height', intendedHeight);
 
 var drawVisual;
 
@@ -49,7 +45,18 @@ if (navigator.getUserMedia) {
         gain.connect(audioCtx.destination);
         //show buttons on audio input confirm
         $('.info').hide();
-        // $('#greenBtn').show();
+        $('#greenBtn').fadeIn();
+        $('#yellowBtn').fadeIn();
+        $('#redBtn').fadeIn();
+        $('#blueBtn').fadeIn();
+        $('#purpleBtn').fadeIn();
+        $('#whiteBtn').fadeIn();
+        $('#funBtn').fadeIn();
+        $('#clearBtn').fadeIn();
+        $('.colorP').fadeIn();
+        $('#title').fadeIn();
+        $('canvas').show();
+
       	visualize();
       },
 
@@ -62,6 +69,47 @@ if (navigator.getUserMedia) {
    console.log('getUserMedia cant be used in your browser!');
 }
 
+//all button clicks for canvas
+$('#greenBtn').click(function(){
+  barColor = 'rgb(' + (20) + ',' + (220) + ',' + (10)+'';
+});
+$('#yellowBtn').click(function(){
+  barColor = 'rgb(' + (250) + ',' + (250) + ',' + (0)+'';
+});
+$('#redBtn').click(function(){
+  barColor = 'rgb(' + (220) + ',' + (20) + ',' + (10)+'';
+});
+$('#blueBtn').click(function(){
+  barColor = 'rgb(' + (0) + ',' + (20) + ',' + (250)+'';
+});
+$('#purpleBtn').click(function(){
+  barColor = 'rgb(' + (220) + ',' + (60) + ',' + (240)+'';
+});
+$('#whiteBtn').click(function(){
+  barColor = 'rgb(' + (250) + ',' + (250) + ',' + (250)+'';
+});
+$('#funBtn').click(function(event){
+  //funMode = 0;
+  console.log(this);
+  // if(canvas.height !== 0){
+    funMode = 0;
+  // }
+  // if(canvas.height === 0){
+  //   funMode = 150;
+  // }
+});
+$('#clearBtn').click(function(){
+  visualize();
+  draw();
+});
+
+//define color variables so they can change
+// var backroundColor = 'rgba(' + 0 +',' + 0 + ',' + 0 + ',' + 1 + ')';
+var barColor = 'rgb(' + (250) + ',' + (250) + ',' + (250)+'';
+var funMode = canvas.height;
+console.log(canvas.height);
+console.log(funMode);
+//spawn the canvas
 function visualize() {
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
@@ -72,16 +120,14 @@ function visualize() {
     var dataArray = new Uint8Array(bufferLength);
 
     canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
+//draw canvas bars
     function draw() {
       drawVisual = requestAnimationFrame(draw);
 
       analyser.getByteFrequencyData(dataArray);
 
-      canvasCtx.fillStyle = 'rgb(' + 0 +',' + 0 + ',' + 0 + ')';
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-      // canvasCtx.arc(75, 50, 50, 0, 2 * Math.PI);// this will stay at top
-
+      canvasCtx.fillStyle = 'rgba(' + 0 +',' + 0 + ',' + 0 + ',' + 1 + ')';
+      canvasCtx.fillRect(0, 0, WIDTH, funMode);
       var barWidth = (WIDTH / bufferLength);
       var barHeight;
       var x = 0;
@@ -89,17 +135,14 @@ function visualize() {
       for(var i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i];
 
+
         //default color//
-        canvasCtx.fillStyle = 'rgb(' + (barHeight+80) + ',' + (30) + ',' + (200)+'';
-      //change color//
-        // $('#greenBtn').click(function(){
-        //   canvasCtx.fillStyle = 'rgb(' + (barHeight) + ',' + (200) + ',' + (20)+'';
-        // })
+        canvasCtx.fillStyle = barColor;
 
         //default shape//
-        canvasCtx.fillRect(x,HEIGHT-barHeight,barWidth,barHeight/4);
+        canvasCtx.fillRect(x, HEIGHT-barHeight, barWidth, barHeight/4);
         x += barWidth;
-          console.log(HEIGHT-barHeight);
+          // console.log(HEIGHT-barHeight);
       }
     }
   draw();
