0a62e4d1dea8537f0f8b7ca6c038b0678c96a3e5
wallytreats <nicklawrenceemail@gmail.com>
2017-05-11 09:30:23 -0600

    site is ready for surge

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var stream;
 
 //audio nodes configuration
 var analyser = audioCtx.createAnalyser();
-analyser.minDecibels = -100;
+analyser.minDecibels = -120;
 analyser.maxDecibels = 20;
 analyser.smoothingTimeConstant = 0.95;
 
